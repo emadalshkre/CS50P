92f35e9be76e430d808c3d0b550e2faee20cbc2a
emad alshkre01~ <emadalshkrejr@gmail.com>
2023-02-19 15:36:01 +0200

    nutrition

---
+++ Week2/nutrition/nutrition.py
@@ -0,0 +1,32 @@
+dic = {
+    "apple": "130",
+    "avocado": "50",
+    "banana": "110",
+    "cantaloupe": "50",
+    "grapefruit": "60",
+    "grapes": "90",
+    "honeydew melon": "50",
+    "kiwifruit": "90",
+    "lemon": "15",
+    "lime": "20",
+    "nectarine": "60",
+    "orange": "80",
+    "peach": "60",
+    "pear": "60",
+    "pineapple": "50",
+    "plums": "70",
+    "strawberries": "50",
+    "sweet cherries": "100",
+    "tangerine": "50",
+    "watermelon": "80"
+}
+while True:
+    fruit = input("Item: ")
+    if fruit.isalpha():
+        fruit = fruit.lower()
+        break
+    else:
+        continue
+
+if fruit in dic:
+    print(dic[fruit])
